735589c73a294af8c52edf026fd11a09f64a8127
HayatoKTYM <hayakap1211@gmail.com>
2023-09-29 08:23:45 +0900

    filter項目を追加

--- src/pages/page1.py
+++ src/pages/page1.py
@@ -2,6 +2,9 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+fig = None
+fig2 = None
+
 pref_data = {
     "Prefecture": ["Tokyo", "Osaka", "Kanagawa"],
     "Population": [10000000, 5000000, 4000000],
@@ -25,6 +28,16 @@ selected_pref = st.selectbox("Select Prefecture:", ["--", "Tokyo", "Osaka", "Kan
 
 num_display = st.selectbox("Number of Bars:", [2, 5, 10])
 
+filter_tokyo = st.sidebar.checkbox("Tokyo", value=True)
+filter_osaka = st.sidebar.checkbox("Osaka", value=True)
+filter_kanagawa = st.sidebar.checkbox("Kanagawa", value=True)
+
+filters = {
+    'Tokyo': filter_tokyo,
+    'Osaka': filter_osaka,
+    'Kanagawa': filter_kanagawa
+}
+
 if selected_pref == "--":  # 都道府県が選択されていない場合
     topN_pref = pref_df.nlargest(num_display, "Population")
     fig = px.bar(
@@ -34,6 +47,16 @@ if selected_pref == "--":  # 都道府県が選択されていない場合
         orientation="h",
         title="Top N Prefectures by Population",
     )
+
+    filtered_data = pref_df[pref_df['Prefecture'].isin([k for k, v in filters.items() if v])]
+    fig2 = px.bar(
+        filtered_data,
+        x="Population",
+        y="Prefecture",
+        orientation="h",
+        title="Filtered Prefectures by Population",
+    )
+
 else:  # 都道府県が選択された場合
     city_df = city_dfs[selected_pref]
     topN_city = city_df.nlargest(num_display, "Population")
@@ -44,11 +67,19 @@ else:  # 都道府県が選択された場合
         orientation="h",
         title=f"Top N Cities in {selected_pref} by Population",
     )
+if fig:
+    fig.update_layout(
+        xaxis_title="Population",
+        yaxis_title="Region",
+        xaxis=dict(tickangle=45),  # x軸のメモリを45度回転
+    )
+    st.plotly_chart(fig)
 
-fig.update_layout(
-    xaxis_title="Population",
-    yaxis_title="Region",
-    xaxis=dict(tickangle=45),  # x軸のメモリを45度回転
-)
+if fig2:
+    fig2.update_layout(
+        xaxis_title="Population",
+        yaxis_title="Region",
+        xaxis=dict(tickangle=45),  # x軸のメモリを45度回転
+    )
+    st.plotly_chart(fig2)
 
-st.plotly_chart(fig)
